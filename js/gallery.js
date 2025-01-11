1ba7e346beb494ef46db4b47042fa5fcd6536092
lucian <luan.tan.nguyen@parcelperform.com>
2025-01-12 01:13:34 +0700

    chore: use static images

--- js/gallery.js
+++ js/gallery.js
@@ -1,31 +1,52 @@
-const remoteGalleryImages = [
-    'https://cms.luciango.com/uploads/ANH_2646_9161be29d2.jpg',
-    'https://cms.luciango.com/uploads/ANH_1602_4fb5443341.jpg',
-    'https://cms.luciango.com/uploads/ANH_2697_05374d554c.jpg',
-    'https://cms.luciango.com/uploads/ANH_1868_b96be9f332.jpg',
-    'https://cms.luciango.com/uploads/ANH_2506_b13b7e6fba.jpg',
-    'https://cms.luciango.com/uploads/ANH_1829_5062c52dc6.jpg',
-    'https://cms.luciango.com/uploads/ANH_2676_bf3a9d79cc.jpg',
-    'https://cms.luciango.com/uploads/ANH_1976_8b0c89af58.jpg',
-    'https://cms.luciango.com/uploads/ANH_2703_941c5c7512.jpg',
-    'https://cms.luciango.com/uploads/ANH_2955_c62ed0d6ed.jpg',
-    'https://cms.luciango.com/uploads/ANH_3001_ad0d8ca358.jpg',
-    'https://cms.luciango.com/uploads/ANH_2437_1_5fc02591b6.jpg',
-    'https://cms.luciango.com/uploads/ANH_3007_705a50e9b5.jpg',
-    'https://cms.luciango.com/uploads/P1664408_e0a2291856.JPG',
-    'https://cms.luciango.com/uploads/P1664457_32e97bc396.JPG',
-    'https://cms.luciango.com/uploads/P1664406_1465c4aad8.JPG',
-    'https://cms.luciango.com/uploads/P1664420_4ed0b1c083.JPG',
-    'https://cms.luciango.com/uploads/P1675186_36c120dc74.JPG'
+const galleryImages = [
+    'ANH_2364.jpg',
+    'ANH_2376.jpg',
+    'ANH_2437_1.jpg',
+    'ANH_2448.jpg',
+    'ANH_2506.jpg',
+    'ANH_2515.jpg',
+    'ANH_2566.jpg',
+    'ANH_2616.jpg',
+    'ANH_2620.jpg',
+    'ANH_2622.jpg',
+    'ANH_2627.jpg',
+    'ANH_2633.jpg',
+    'ANH_2637.jpg',
+    'ANH_2646.jpg',
+    'ANH_2676.jpg',
+    'ANH_2697.jpg',
+    'ANH_2703.jpg',
+    'ANH_2871.jpg',
+    'ANH_2955.jpg',
+    'ANH_3001.jpg',
+    'ANH_3007.jpg',
+    'P1664406.JPG',
+    'P1664408.JPG',
+    'P1664420.JPG',
+    'P1664437.JPG',
+    'P1664457.JPG',
+    'P1664503.JPG',
+    'P1675006.JPG',
+    'P1675011.JPG',
+    'P1675030.JPG',
+    'P1675040.JPG',
+    'P1675063.JPG',
+    'P1675089.JPG',
+    'P1675095.JPG',
+    'P1675129.JPG',
+    'P1675155.JPG',
+    'P1675171.JPG',
+    'P1675179.JPG',
+    'P1675186.JPG',
 ];
-const IMAGES_PER_LOAD = 6;
+const IMAGES_PER_LOAD = 12;
 let currentLoadedIndex = 0;
 
 function createGalleryItem(imageName) {
     return `
         <div class="gallery-item" data-animate-effect="fadeIn">
-            <a href="${imageName}" class="gallery-popup">
-                <img src="${imageName}" alt="Gallery Image">
+            <a href="images/gallery/${imageName}" class="gallery-popup">
+                <img src="images/gallery/${imageName}" alt="Gallery Image">
             </a>
         </div>
     `;
@@ -35,7 +56,7 @@ const INITIAL_IMAGES_COUNT = 12; // Show first 12 images initially
 
 function initializeGallery() {
     const container = document.getElementById('gallery-container');
-    const initialImages = remoteGalleryImages.slice(0, INITIAL_IMAGES_COUNT);
+    const initialImages = galleryImages.slice(0, INITIAL_IMAGES_COUNT);
     const galleryHTML = initialImages.map(createGalleryItem).join('');
     container.innerHTML = galleryHTML;
     currentLoadedIndex = INITIAL_IMAGES_COUNT;
@@ -44,7 +65,7 @@ function initializeGallery() {
 
 function showMoreImages() {
     const container = document.getElementById('gallery-container');
-    const nextImages = remoteGalleryImages.slice(currentLoadedIndex, currentLoadedIndex + IMAGES_PER_LOAD).map(createGalleryItem).join('');
+    const nextImages = galleryImages.slice(currentLoadedIndex, currentLoadedIndex + IMAGES_PER_LOAD).map(createGalleryItem).join('');
     container.innerHTML += nextImages;
     currentLoadedIndex += IMAGES_PER_LOAD;
     updateShowMoreButton();
@@ -52,7 +73,7 @@ function showMoreImages() {
 
 function updateShowMoreButton() {
     const showMoreBtn = document.getElementById('showMoreBtn');
-    if (currentLoadedIndex >= remoteGalleryImages.length) {
+    if (currentLoadedIndex >= galleryImages.length) {
         showMoreBtn.style.display = 'none';
     }
 }
