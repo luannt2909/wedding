6383c6e231ef99e2111b3f4e783f5f305bb2991c
lucian <luan.tan.nguyen@parcelperform.com>
2025-02-03 22:45:47 +0700

    chg: adjust code

--- js/gallery.js
+++ js/gallery.js
@@ -1,45 +1,75 @@
+// const galleryImages = [
+//     'ANH_2364.jpg',
+//     'ANH_2376.jpg',
+//     'ANH_2437_1.jpg',
+//     'ANH_2448.jpg',
+//     'ANH_2506.jpg',
+//     'ANH_2515.jpg',
+//     'ANH_2566.jpg',
+//     'ANH_2616.jpg',
+//     'ANH_2620.jpg',
+//     'ANH_2622.jpg',
+//     'ANH_2627.jpg',
+//     'ANH_2633.jpg',
+//     'ANH_2637.jpg',
+//     'ANH_2646.jpg',
+//     'ANH_2676.jpg',
+//     'ANH_2697.jpg',
+//     'ANH_2703.jpg',
+//     'ANH_2871.jpg',
+//     'ANH_2955.jpg',
+//     'ANH_3001.jpg',
+//     'ANH_3007.jpg',
+//     'P1664406.JPG',
+//     'P1664408.JPG',
+//     'P1664420.JPG',
+//     'P1664437.JPG',
+//     'P1664457.JPG',
+//     'P1664503.JPG',
+//     'P1675006.JPG',
+//     'P1675011.JPG',
+//     'P1675030.JPG',
+//     'P1675040.JPG',
+//     'P1675063.JPG',
+//     'P1675089.JPG',
+//     'P1675095.JPG',
+//     'P1675129.JPG',
+//     'P1675155.JPG',
+//     'P1675171.JPG',
+//     'P1675179.JPG',
+//     'P1675186.JPG',
+// ];
 const galleryImages = [
-    'ANH_2364.jpg',
-    'ANH_2376.jpg',
-    'ANH_2437_1.jpg',
-    'ANH_2448.jpg',
-    'ANH_2506.jpg',
-    'ANH_2515.jpg',
-    'ANH_2566.jpg',
-    'ANH_2616.jpg',
-    'ANH_2620.jpg',
-    'ANH_2622.jpg',
-    'ANH_2627.jpg',
-    'ANH_2633.jpg',
-    'ANH_2637.jpg',
-    'ANH_2646.jpg',
-    'ANH_2676.jpg',
-    'ANH_2697.jpg',
-    'ANH_2703.jpg',
-    'ANH_2871.jpg',
+    // group 1
+    'ANH_2324.jpg', //ngang
+    'ANH_1990.jpg',
+    'ANH_2055.jpg',
+    'tanhon_4.jpg', //ngang
+    'tanhon_7.jpg',
     'ANH_2955.jpg',
-    'ANH_3001.jpg',
-    'ANH_3007.jpg',
-    'P1664406.JPG',
-    'P1664408.JPG',
-    'P1664420.JPG',
-    'P1664437.JPG',
-    'P1664457.JPG',
+    // group 2
+    'ZS_184.JPG',
+    'ZS_158.JPG', //ngang
+    'tanhon_3.jpg',
+    'ANH_2437.jpg',
+    'P1675171.JPG', //ngang
+    'ANH_2622.jpg',
+    // group 3
     'P1664503.JPG',
-    'P1675006.JPG',
-    'P1675011.JPG',
-    'P1675030.JPG',
-    'P1675040.JPG',
-    'P1675063.JPG',
-    'P1675089.JPG',
-    'P1675095.JPG',
-    'P1675129.JPG',
-    'P1675155.JPG',
-    'P1675171.JPG',
-    'P1675179.JPG',
-    'P1675186.JPG',
+    'ZS_149.JPG',
+    'ZS_155.JPG',
+    'P1675011.JPG', //ngang
+    'tanhon_2.jpg',
+    'tanhon_6.jpg', //ngang
+    // group 4
+    'ANH_2697.jpg', //ngang
+    'ANH_2566.jpg',
+    'tanhon_5.jpg',
+    'tanhon_8.jpg', //ngang
+    'ZS_135.JPG',
+    'ANH_2506.jpg',
 ];
-const IMAGES_PER_LOAD = 12;
+const IMAGES_PER_LOAD =24;
 let currentLoadedIndex = 0;
 
 function createGalleryItem(imageName) {
@@ -52,7 +82,7 @@ function createGalleryItem(imageName) {
     `;
 }
 
-const INITIAL_IMAGES_COUNT = 12; // Show first 12 images initially
+const INITIAL_IMAGES_COUNT = 24; // Show first 12 images initially
 
 function initializeGallery() {
     const container = document.getElementById('gallery-container');
